4bae2c5ce66525714e355373fbd3eb5bffe3cb33
Baek <trumpetbaek@gmail.com>
2022-01-07 10:42:34 +0900

    seat.js 22-01-07

--- team/src/main/webapp/js/seat.js
+++ team/src/main/webapp/js/seat.js
@@ -1,54 +1,58 @@
 /**
  * 
  */
-let test = [];
 let selectedSeatsArray = new Array();
-const seatWrapper = document.querySelector('.seat-wrapper');
+let seatWrapper = document.querySelector('.seat-wrapper');
 let clicked = '';
 let div = '';
 let seat = document.querySelectorAll('.seat');
-const selectedSeats = document.querySelector('.selected-seats');
-const allSeat = document.querySelector('.all-seats');
-const remainSeat = document.querySelector('.remain-seats');
-const reserveNumber = document.querySelector('.reserve-number');
-const selectSeatListNormal = document.querySelectorAll(
-	'.select-seat-ul-normal li'
+let selectedSeats = document.querySelector('.selected-seats');
+let allSeat = document.querySelector('.all-seats');
+let remainSeat = document.querySelector('.remain-seats');
+let remainSeats = document.querySelector('.remainSeat');
+let reserveNumber = document.querySelector('.reserve-number');
+let selectSeatListSeatR = document.querySelectorAll(
+	'.select-seat-ul-seatR li'
 );
-const selectSeatListTeen = document.querySelectorAll('.select-seat-ul-teen li');
-const selectSeatListOld = document.querySelectorAll('.select-seat-ul-old li');
+let selectSeatListSeatS = document.querySelectorAll('.select-seat-ul-seatS li');
+let selectSeatListSeatA = document.querySelectorAll('.select-seat-ul-seatA li');
 let selectSeatListUlActive = '';
-let normalNumber = 0;
-let teenNumber = 0;
-let oldNumber = 0;
+let seatRNumber = 0;
+let seatSNumber = 0;
+let seatANumber = 0;
 let allNumber = 0;
 let prevNumber = 0;
-let normalMoney = 0;
-let teenMoney = 0;
-let oldMoney = 0;
+let seatRMoney = 0;
+let seatSMoney = 0;
+let seatAMoney = 0;
 let allMoney = 0;
 
-const selectNumberNormal = document.querySelectorAll('.select-number-normal');
-const selectNumberTeen = document.querySelectorAll('.select-number-teen');
-const selectNumberOld = document.querySelectorAll('.select-number-old');
+let checkRNumber = 0;
+let checkSNumber = 0;
+let checkANumber = 0;
+
+let selectNumberSeatR = document.querySelectorAll('.select-number-seatR');
+let selectNumberSeatS = document.querySelectorAll('.select-number-seatS');
+let selectNumberSeatA = document.querySelectorAll('.select-number-seatA');
 
 //예약 관련
-const selectedPlay = document.querySelector('.selected-play');
-const selectedTheaterPlaceInfo = document.querySelectorAll(
+let selectedPlay = document.querySelector('.selected-play');
+let selectedTheaterPlaceInfo = document.querySelectorAll(
 	'.selected-theater-place-info'
 );
-const theaterTime = document.querySelector('.theater-time');
-const theaterDate = document.querySelector('.theater-date');
-const ticketPrice = document.querySelector('.ticket-price');
-const payMoney = document.querySelector('.payMoney');
+let theaterTime = document.querySelector('.theater-time');
+let theaterDate = document.querySelector('.theater-date');
+let ticketPrice = document.querySelector('.ticket-price');
+let payMoney = document.querySelector('.payMoney');
 
-const seatForm = document.querySelector('.seatForm');
-const reserveButton = document.querySelector('.reserve-button');
-const title = document.querySelector('.title');
-const selectedTheater = document.querySelector('.selectedTheater');
-const reserveDate = document.querySelector('.reserveDate');
-const runningTime = document.querySelector('.runningTime');
-const ticketNumber = document.querySelector('.ticketNumber');
-const selectedSeat = document.querySelector('.selectedSeat');
+let seatForm = document.querySelector('.seatForm');
+let reserveButton = document.querySelector('.reserve-button');
+let title = document.querySelector('.title');
+let selectedTheater = document.querySelector('.selectedTheater');
+let reserveDate = document.querySelector('.reserveDate');
+let runningTime = document.querySelector('.runningTime');
+let ticketNumber = document.querySelector('.ticketNumber');
+let selectedSeat = document.querySelector('.selectedSeat');
 
 //버튼클릭시 다른 class 추가 초기화해주기
 function selectSeatList(list) {
@@ -73,9 +77,9 @@ function initList(list, li) {
 		selectListUiFunction(selectSeatListUlActive);
 		console.log('금액 : ' + allMoney);
 
-		console.log("normalNumber : " + normalNumber);
-		console.log("teenNumber : " + teenNumber);
-		console.log("oldNumber : " + oldNumber);
+		console.log("seatRNumber : " + seatRNumber);
+		console.log("seatSNumber : " + seatSNumber);
+		console.log("seatANumber : " + seatANumber);
 		console.log("allNumber : " + allNumber);
 	});
 }
@@ -83,72 +87,57 @@ function initList(list, li) {
 function selectListUiFunction(selectSeatListUlActive) {
 	selectSeatListUlActive.forEach(function(li) {
 
-		if (li.parentNode.classList.contains('select-seat-ul-normal')) {
-			normalNumber = Number(li.innerHTML);
-			normalMoney = 11000 * normalNumber;
-			allMoney = normalMoney + teenMoney + oldMoney;
-			allNumber = normalNumber + teenNumber + oldNumber;
+		if (li.parentNode.classList.contains('select-seat-ul-seatR')) {
+			seatRNumber = Number(li.innerHTML);
+			seatRMoney = 20000 * seatRNumber;
+			allMoney = seatRMoney + seatSMoney + seatAMoney;
+			allNumber = seatRNumber + seatSNumber + seatANumber;
 			ticketPrice.innerHTML = allMoney + '원';
 			if (allNumber > 16) {
 				li.classList.remove('select-seat-ul-active');
-				allMoney -= normalMoney;
-				allNumber = allNumber - normalNumber;
+				allMoney -= seatRMoney;
+				allNumber = allNumber - seatRNumber;
 
-				selectNumberNormal[0].classList.add('select-seat-ul-active');
+				selectNumberSeatR[0].classList.add('select-seat-ul-active');
 				ticketPrice.innerHTML = allMoney + '원';
 			}
-		} else if (li.parentNode.classList.contains('select-seat-ul-teen')) {
-			teenNumber = Number(li.innerHTML);
-			teenMoney = 8000 * teenNumber;
-			allMoney = normalMoney + teenMoney + oldMoney;
-			allNumber = normalNumber + teenNumber + oldNumber;
+		} else if (li.parentNode.classList.contains('select-seat-ul-seatS')) {
+			seatSNumber = Number(li.innerHTML);
+			seatSMoney = 8000 * seatSNumber;
+			allMoney = seatRMoney + seatSMoney + seatAMoney;
+			allNumber = seatRNumber + seatSNumber + seatANumber;
 			ticketPrice.innerHTML = allMoney + '원';
 			if (allNumber > 16) {
 				li.classList.remove('select-seat-ul-active');
-				//normalNumber = teenNumber = oldNumber = 0;
-				allMoney -= teenMoney;
-				allNumber = allNumber - teenNumber;
+				//seatRNumber = seatSNumber = seatANumber = 0;
+				allMoney -= seatSMoney;
+				allNumber = allNumber - seatSNumber;
 
-				selectNumberTeen[0].classList.add('select-seat-ul-active');
+				selectNumberSeatS[0].classList.add('select-seat-ul-active');
 				ticketPrice.innerHTML = allMoney + '원';
 			}
-		} else if (li.parentNode.classList.contains('select-seat-ul-old')) {
-			oldNumber = Number(li.innerHTML);
-			oldMoney = 8000 * oldNumber;
-			allMoney = normalMoney + teenMoney + oldMoney;
-			allNumber = normalNumber + teenNumber + oldNumber;
+		} else if (li.parentNode.classList.contains('select-seat-ul-seatA')) {
+			seatANumber = Number(li.innerHTML);
+			seatAMoney = 8000 * seatANumber;
+			allMoney = seatRMoney + seatSMoney + seatAMoney;
+			allNumber = seatRNumber + seatSNumber + seatANumber;
 			ticketPrice.innerHTML = allMoney + '원';
-			if (allNumber > 16) {
-				li.classList.remove('select-seat-ul-active');
-
-				allMoney -= oldMoney;
-				allNumber = allNumber - oldNumber;
-
-				selectNumberOld[0].classList.add('select-seat-ul-active');
-				ticketPrice.innerHTML = allMoney + '원';
-			}
 		}
 
 
-		// allNumber = normalNumber + teenNumber + oldNumber;
-		// allMoney = normalMoney + teenMoney + oldMoney;
+		// allNumber = seatRNumber + seatSNumber + seatANumber;
+		// allMoney = seatRMoney + seatSMoney + seatAMoney;
 		console.log(allNumber + '명');
 		ticketPrice.innerHTML = allMoney + '원';
 
-		if (allNumber > 16) {
-			console.log(li);
-			li.classList.remove('select-seat-ul-active');
-			// normalNumber = teenNumber = oldNumber = 0;
-			alert("지정한 인원수를 넘었습니다(최대 8명)");
-		}
 		payMoney.value = allMoney;
 		console.log(allMoney);
 	});
 }
 
-selectSeatList(selectSeatListNormal);
-selectSeatList(selectSeatListTeen);
-selectSeatList(selectSeatListOld);
+selectSeatList(selectSeatListSeatR);
+selectSeatList(selectSeatListSeatS);
+selectSeatList(selectSeatListSeatA);
 
 for (let i = 1; i < 11; i++) {
 	div = document.createElement('div');
@@ -156,7 +145,7 @@ for (let i = 1; i < 11; i++) {
 	seatWrapper.append(div);
 
 	for (let j = 1; j < 13; j++) {
-		const input = document.createElement('input');
+		let input = document.createElement('input');
 		input.type = 'button';
 		input.name = 'seats';
 		input.classList = 'seat';
@@ -172,11 +161,11 @@ for (let i = 1; i < 11; i++) {
 	allSeat.innerHTML = seat.length;
 }
 
-seat.forEach(data => {
+seat.forEach(function(data) {
 	//console.log(data.value.substring(1, data.value.length));
 	// console.log(data.value.substring(0, data.value.length - 1));
-	//좌석이 2나 9로 끝나는얘들은 왼쪽이나 오른쪽으로 띄워주기위한 class추가
-	if (data.value.substring(1, data.value.length) == '3') {
+	if (data.value.substring(1, data.value.length) == '3' |
+		data.value.substring(1, data.value.length) == '7') {
 		data.classList.add('left-margin');
 	} else if (data.value.substring(1, data.value.length) == '10') {
 		data.classList.add('right-margin');
@@ -193,6 +182,65 @@ seat.forEach(data => {
 function inputClickEvent(input) {
 	input.addEventListener('click', function(e) {
 		console.log("e.target.value : " + e.target.value);
+
+		let checkRSeat = e.target.value.substring(0, e.target.value.length - 1) == 'A' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'A' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'B' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'B' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'C' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'C';
+
+		let checkSSeat = e.target.value.substring(0, e.target.value.length - 1) == 'D' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'D' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'E' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'E' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'F' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'F';
+
+		let checkASeat = e.target.value.substring(0, e.target.value.length - 1) == 'G' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'G' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'H' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'H' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'I' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'I' ||
+			e.target.value.substring(0, e.target.value.length - 1) == 'J' ||
+			e.target.value.substring(0, e.target.value.length - 2) == 'J';
+
+		if (checkRSeat) {
+			if (seatRNumber < 1) {
+				alert("R석 입니다.");
+				return;
+			} else if (checkRNumber < seatRNumber && seatRNumber >= 1) {
+				checkRNumber += 1;
+				console.log("insertCheckRNumber:" + checkRNumber);
+			} else {
+				alert("이미 좌석을 선택하셨습니다.");
+				return;
+			}
+		}
+		if (checkSSeat) {
+			if (seatSNumber < 1) {
+				alert("S석 입니다.");
+				return;
+			} else if (checkSNumber < seatSNumber && seatSNumber >= 1) {
+				checkSNumber += 1;
+			} else {
+				alert("이미 좌석을 선택하셨습니다.");
+				return;
+			}
+		}
+		if (checkASeat) {
+			if (seatANumber < 1) {
+				alert("A석 입니다.");
+				return;
+			} else if (checkANumber < seatANumber && seatANumber >= 1) {
+				checkANumber += 1;
+			} else {
+				alert("이미 좌석을 선택하셨습니다.");
+				return;
+			}
+		}
+
 		//중복방지 함수
 		selectedSeatsArray = selectedSeatsArray.filter(
 			function(element, index) {
@@ -201,21 +249,32 @@ function inputClickEvent(input) {
 
 		//click class가 존재할때(제거해주는 toggle)
 		if (input.classList.contains('clicked')) {
+
+			if (checkRSeat) {
+				checkRNumber -= 2;
+				console.log("delCheckRNumber:" + checkRNumber);
+			} else if (checkSSeat) {
+				checkSNumber -= 2;
+			} else if (checkASeat) {
+				checkANumber -= 2;
+			}
+
 			input.classList.remove('clicked');
 			clicked = document.querySelectorAll('.clicked');
-
 			//배열안의 값 제거
 			selectedSeatsArray.splice(selectedSeatsArray.indexOf(e.target.value), 1);
 			clicked.forEach(function(data) {
 				selectedSeatsArray.push(data.value);
 			});
+
+
 			//class가 존재안할때 추가해주는 toggle
 		} else {
 			input.classList.add('clicked');
 			clicked = document.querySelectorAll('.clicked');
 			//선택한 번호의 갯수를 넘기면 동작 못하게 하는 코드
 			console.log("allNumber : " + allNumber);
-			if (clicked.length > allNumber) {
+			if (clicked.length > 8) {
 				input.classList.remove('clicked');
 				alert("지정한 인원수를 넘었습니다(최대 8명)");
 				return;
@@ -275,6 +334,7 @@ reserveButton.addEventListener('click', function() {
 	runningTime.value = theaterTime.innerHTML;
 	ticketNumber.value = reserveNumber.innerHTML;
 	selectedSeat.value = selectedSeats.innerHTML;
+	remainSeats.value = remainSeat.innerHTML;
 	console.log(allNumber + '임');
 	console.log(ticketNumber.value);
 	console.log(allNumber == ticketNumber.value);
@@ -287,6 +347,6 @@ reserveButton.addEventListener('click', function() {
 		seatForm.action = 'payment';
 		seatForm.submit();
 	} else {
-		alert('좌석을 모두선택해 주세요!');
+		alert('좌석을 모두선택해 주세요.');
 	}
 });
